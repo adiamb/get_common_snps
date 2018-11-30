797e54093557efd4ecdea9fa3e7afd473b108601
Aditya Ambati <ambati@stanford.edu>
2018-11-30 10:25:08 -0800

    updated arg parsing to take an output file

--- extract_common_snps.py
+++ extract_common_snps.py
@@ -2,8 +2,9 @@ import sys
 import time
 import argparse
 parser = argparse.ArgumentParser()
-#parser.add_argument('-1', '--list', nargs='+', help='<Required> Set flag', required=True)
+#parser.add_argument('-l', '--list', nargs='+', help='input a list of snplists from different GWAS batches', required=True)
 parser.add_argument('file', type=argparse.FileType('r'), nargs='+')
+parser.add_argument('-out',required=True, help='outfile a stringname with a suffix .txt') 
 def snp_extract(snplist):
 	global snp_
 	snp_={}
@@ -27,7 +28,7 @@ for n, value in enumerate(args.file):
 
 n=0
 missing =0
-with open('SNPLIST_COMMON.txt', 'w') as f_out:
+with open(args.out, 'w') as f_out:
 	for key in all_snp[0].iterkeys():
 		if all(key in d for d in dic_list):
 			print key
@@ -38,23 +39,3 @@ with open('SNPLIST_COMMON.txt', 'w') as f_out:
 			missing += 1
 
 print 'matched ', str(n), 'SNPS and Discarded other ', str(missing), 'SNPS' 
-
-
-#if key in dic_list[0] and dic_list[1] and dic_list[2]:
-	#print 'yeay' 
-
-
-
-#args_file=['CHB1_EXTRACT_P76.snplist',#'Axiom_550606_CHB1_merged_no68XYM_remR1_filterOUT.snplist']
-#'CHB1_EXTRACT_P89.snplist', 
-#'CHB1_EXTRACT_P94_95.snplist']
-
-#def snp_extract(snplist):
-#	global snp_
-#	snp_={}
-#	with open(snplist) as f_in:
-#		for line in f_in:
-#			line_parse =  line.strip()
-#			if line_parse not in snp_:
-#				snp_[line_parse] = ''
-#	return(snp_)
