b25de701becd254e0cfd2375449c77f6a53bd046
Aditya Ambati <ambati@stanford.edu>
2018-12-06 17:25:41 -0800

    Update extract_common_snps.py

--- extract_common_snps.py
+++ extract_common_snps.py
@@ -3,7 +3,9 @@ import time
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('file', type=argparse.FileType('r'), nargs='+', help='input a list of snplists from different GWAS batches', required=True)
-parser.add_argument('-out',required=True, help='outfile a stringname with a suffix .txt') 
+parser.add_argument('-out',required=True, help='outfile a stringname with a suffix .txt')
+args=parser.parse_args()
+
 def snp_extract(snplist):
 	global snp_
 	snp_={}
@@ -13,8 +15,7 @@ def snp_extract(snplist):
 			snp_[line_parse] = ''
 	return(snp_)
 
-#def main(sys.argv[1]):
-#	pass
+
 all_snp=[]
 dic_list =[]
 args = parser.parse_args()
