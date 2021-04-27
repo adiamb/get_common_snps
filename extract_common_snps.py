09580440780d85a997b4906f804222f594a51d4b
Aditya Ambati <ambati@stanford.edu>
2021-04-27 16:07:04 -0700

    Update extract_common_snps.py

--- extract_common_snps.py
+++ extract_common_snps.py
@@ -1,40 +1,38 @@
-import sys
-import time
 import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('file', type=argparse.FileType('r'), nargs='+', help='input a list of snplists from different GWAS batches')
-parser.add_argument('-out',required=True, help='outfile a stringname with a suffix .txt')
+from collections import defaultdict
 
-def snp_extract(snplist):
-	global snp_
-	snp_={}
-	for line in snplist:
-		line_parse =  line.strip()
-		if line_parse not in snp_:
-			snp_[line_parse] = ''
-	return(snp_)
+def processSnplist(fileHandles):
+    nFiles = len(fileHandles)
+    snpDict = defaultdict(int)
+    for fileC, snpFile in enumerate(fileHandles):
+        for snp in snpFile:
+            snpParse = snp.strip()
+            snpDict[snpParse] += 1
+    return(snpDict)
+
+def processOut(out, snpDict, nFiles):
+    snpCounter = 0
+    with open(out, 'w') as outFile:
+        for snp, val in snpDict.items():
+            if val == nFiles:
+                snpCounter += 1
+                outFile.write(snp+'\n')
+    if snpCounter > 0:
+        print('MATCHED {} SNPS ACROSS {} BEDFILES'.format(snpCounter, nFiles))
+    else:
+        print('UNABLE TO FIND CONSENSUS AMONG THE SNPLISTS')
 
 
-all_snp=[]
-dic_list =[]
-args = parser.parse_args()
-for n, value in enumerate(args.file):
-	if n > 0:
-		if value is not None:
-			dic_list.append(snp_extract(snplist=value))
-	else:
-		all_snp.append(snp_extract(snplist=value))
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', type=argparse.FileType('r'), nargs='+', help='input a list of snplists from different GWAS batches')
+    parser.add_argument('-out',required=True, help='outfile a stringname with a suffix .txt')
+    args = parser.parse_args()
+    fileHandles = args.file
+    out = args.out
+    nFiles = len(fileHandles)
+    print('FOUND {} SNPLISTS'.format(nFiles))
+    snpDict=processSnplist(fileHandles)
+    processOut(out, snpDict, nFiles)
 
-n=0
-missing =0
-with open(args.out, 'w') as f_out:
-	for key in all_snp[0].keys():
-		if all(key in d for d in dic_list):
-			print(key)
-			n += 1
-			f_out.write(key+'\n')
-		else:
-			#print('to throw')
-			missing += 1
-
-print('matched  {} SNPS and Discarded other {} SNPS'.format(n, missing))
+if __name__ == '__main__':main()
